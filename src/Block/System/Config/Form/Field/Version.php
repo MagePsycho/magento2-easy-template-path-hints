cbdf871d81a965f94a6e8d274e3c4824c667ea21
Raj KB <magepsycho@gmail.com>
2016-04-22 21:22:21 +0400

    Formatted the code as per Code Sniffer

--- src/Block/System/Config/Form/Field/Version.php
+++ src/Block/System/Config/Form/Field/Version.php
@@ -10,40 +10,42 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
  * @package    MagePsycho_Easypathhints
  * @author     Raj KB <magepsycho@gmail.com>
  * @website    http://www.magepsycho.com
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Version extends \Magento\Config\Block\System\Config\Form\Field
 {
-	const EXTENSION_URL = 'http://www.magepsycho.com/extensions.html';
+    const EXTENSION_URL = 'http://www.magepsycho.com/extensions.html';
 
-	/**
-	 * @var \MagePsycho\Easypathhints\Helper\Data $helper
-	 */
-	protected $_helper;
+    /**
+     * @var \MagePsycho\Easypathhints\Helper\Data $helper
+     */
+    protected $_helper;
 
-	/**
-	 * @param \Magento\Backend\Block\Template\Context $context
-	 * @param \MagePsycho\Easypathhints\Helper\Data $helper
-	 */
-	public function __construct(
-		\Magento\Backend\Block\Template\Context $context,
-		\MagePsycho\Easypathhints\Helper\Data $helper
-	) {
-		$this->_helper = $helper;
-		parent::__construct($context);
-	}
+    /**
+     * @param   \Magento\Backend\Block\Template\Context $context
+     * @param   \MagePsycho\Easypathhints\Helper\Data   $helper
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \MagePsycho\Easypathhints\Helper\Data $helper
+    ) {
+        $this->_helper = $helper;
+        parent::__construct($context);
+    }
 
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $extensionVersion = $this->_helper->getExtensionVersion();
+        $versionLabel     = sprintf(
+            '<a href="%s" title="Easy Template Path Hints" target="_blank">%s</a>',
+            self::EXTENSION_URL,
+            $extensionVersion
+        );
+        $element->setValue($versionLabel);
 
-	/**
-	 * @param AbstractElement $element
-	 * @return string
-	 */
-	protected function _getElementHtml(AbstractElement $element)
-	{
-		$extensionVersion   = $this->_helper->getExtensionVersion();
-		$versionLabel       = sprintf('<a href="%s" title="Easy Template Path Hints for Magento 2" target="_blank">%s</a>', self::EXTENSION_URL, $extensionVersion);
-		$element->setValue($versionLabel);
-
-		return $element->getValue();
-	}
+        return $element->getValue();
+    }
 }
--- src/Observer/EasyTemplateHintsObserver.php
+++ src/Observer/EasyTemplateHintsObserver.php
@@ -3,6 +3,9 @@ namespace MagePsycho\Easypathhints\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\App\Config\MutableScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use MagePsycho\Easypathhints\Helper\Data as EasypathhintsHelper;
 
 /**
  * Observer Class
@@ -11,46 +14,59 @@ use Magento\Framework\Event\ObserverInterface;
  * @package    MagePsycho_Easypathhints
  * @author     Raj KB <magepsycho@gmail.com>
  * @website    http://www.magepsycho.com
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class EasyTemplateHintsObserver implements ObserverInterface
 {
 
-	/**
-	 * @var \MagePsycho\Easypathhints\Helper\Data $helper
-	 */
-	protected $_helper;
+    /**
+     * @var EasypathhintsHelper $helper
+     */
+    protected $_helper;
 
-	/**
-	 * @var \Magento\Framework\App\Config\MutableScopeConfigInterface
-	 */
-	protected $_mutableConfig;
+    /**
+     * @var MutableScopeConfigInterface
+     */
+    protected $_mutableConfig;
 
-	/**
-	 * @param \MagePsycho\Easypathhints\Helper\Data $helper
-	 * @param \Magento\Framework\App\Config\MutableScopeConfigInterface $mutableConfig
-	 */
-	public function __construct(
-		\MagePsycho\Easypathhints\Helper\Data $helper,
-		\Magento\Framework\App\Config\MutableScopeConfigInterface $mutableConfig
-	) {
-		$this->_helper          = $helper;
-		$this->_mutableConfig   = $mutableConfig;
-	}
+    /**
+     * @param EasypathhintsHelper $helper
+     * @param MutableScopeConfigInterface $mutableConfig
+     */
+    public function __construct(
+        EasypathhintsHelper $helper,
+        MutableScopeConfigInterface $mutableConfig
+    ) {
+        $this->_helper          = $helper;
+        $this->_mutableConfig   = $mutableConfig;
+    }
 
-	public function execute(\Magento\Framework\Event\Observer $observer)
-	{
-		#$this->_helper->log($observer->getEvent()->getName(), true);
-		if ($this->_helper->shouldShowTemplatePathHints()) {
+    public function execute(Observer $observer)
+    {
+        $this->_helper->log($observer->getEvent()->getName(), true);
+        if ($this->_helper->shouldShowTemplatePathHints()) {
 
-			$this->_mutableConfig->setValue(\MagePsycho\Easypathhints\Helper\Data::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_STOREFRONT, 1, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-			$this->_mutableConfig->setValue(\MagePsycho\Easypathhints\Helper\Data::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_ADMIN, 1, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-			$this->_mutableConfig->setValue(\MagePsycho\Easypathhints\Helper\Data::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_BLOCKS, 1, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $this->_mutableConfig->setValue(
+                EasypathhintsHelper::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_STOREFRONT,
+                1,
+                ScopeInterface::SCOPE_STORE
+            );
 
-			if ($this->_helper->getShowProfiler()) {
-				$_SERVER['MAGE_PROFILER'] = 'html';
-			}
-		}
-		return $this;
-	}
+            $this->_mutableConfig->setValue(
+                EasypathhintsHelper::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_ADMIN,
+                1,
+                ScopeInterface::SCOPE_STORE
+            );
+
+            $this->_mutableConfig->setValue(
+                EasypathhintsHelper::XML_PATH_DEV_DEBUG_TEMPLATE_HINTS_BLOCKS,
+                1,
+                ScopeInterface::SCOPE_STORE
+            );
+
+            /*if ($this->_helper->getShowProfiler()) {
+                $_SERVER['MAGE_PROFILER'] = 'html';
+            }*/
+        }
+        return $this;
+    }
 }
